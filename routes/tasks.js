79371116ec5ec3ec247691fe0a52fc59ef028119
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-09-22 00:22:09 +0200

    an error message and an error status corrected

--- routes/tasks.js
+++ routes/tasks.js
@@ -30,12 +30,15 @@ router.get("/", async(req,res) => {
 router.get("/id/:_id", async(req,res) => {
     try {
         const task = await Task.findById(req.params._id);
+        if (!task) {
+            return res.status(404).send({ message: "The task with the provided id does not exist" })
+        }
         res.status(201).send(task);
     } catch (error) {
         console.error(error);
         res
             .status(500)
-            .send({ message: "The task with the provided id does not exist" });
+            .send({ message: "An error occurred while fetching the task" });
     }
 })
 
